fd6456fe0e490df957a37aa1379d287aeb4bfa8e
CARobotics619 <carobotics@gmail.com>
2014-01-19 17:45:40 -0500

    Added aforge rectangle detection

---
+++ DSVision/MainWindow.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DSVision
+{
+    public partial class MainWindow : Form
+    {
+        public MainWindow(Bitmap bmp)
+        {
+            InitializeComponent();
+
+            picture.Image = bmp;
+        }
+    }
+}
---
+++ DSVision/Program.cs
@@ -0,0 +1,56 @@
+﻿using AForge;
+using AForge.Imaging;
+using AForge.Math.Geometry;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DSVision
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Bitmap bmp = new Bitmap("Test.png");
+            BlobCounter blobCounter = new BlobCounter();
+            blobCounter.ProcessImage(bmp);
+            Blob[] blobs = blobCounter.GetObjectsInformation();
+            SimpleShapeChecker shapeChecker = new SimpleShapeChecker();
+            Graphics g = Graphics.FromImage(bmp);
+            Pen redPen = new Pen(Color.Red, 2);
+            foreach (Blob blob in blobs)
+            {
+                List<IntPoint> edgePoints = blobCounter.GetBlobsEdgePoints(blob);
+                List<IntPoint> corners;
+                if (edgePoints.Count == 1)
+                {
+                    continue;
+                }
+
+                if (shapeChecker.IsQuadrilateral(edgePoints, out corners))
+                {
+                    g.DrawPolygon(redPen, ToPointsArray(corners));
+                }
+            }
+            Application.Run(new MainWindow(bmp));
+            redPen.Dispose();
+            g.Dispose();
+        }
+        
+        public static System.Drawing.Point[] ToPointsArray(List<IntPoint> points)
+        {
+            System.Drawing.Point[] array = new System.Drawing.Point[points.Count];
+
+            for (int i = 0, n = points.Count; i < n; i++)
+            {
+                array[i] = new System.Drawing.Point(points[i].X, points[i].Y);
+            }
+
+            return array;
+        }
+    }
+}
